24b9d0b5042554a656fe655eddf23c05b08fb534
DerTomm <de.thomas.reimer@gmail.com>
2026-09-02 22:01:46 +0200

    refactor: update build.gradle and eas.json for improved configuration and remove app.config.js

--- app/app.config.js
+++
@@ -1,9 +0,0 @@
-const appJson = require('./app.json');
-
-module.exports = {
-    ...appJson.expo,
-    android: {
-        ...appJson.expo.android,
-        googleServicesFile: process.env.GOOGLE_SERVICES_JSON || './android/app/google-services.json',
-    },
-};
